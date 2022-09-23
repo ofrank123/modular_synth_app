41571a8a3af897eb007f1980bc39633a83bd198d
Oliver Frank <oliverfrank321@gmail.com>
2022-09-23 01:49:07 +0100

    More dinking around with the buffer.

--- wasm-mod-synth/src/buffer.rs
+++ wasm-mod-synth/src/buffer.rs
@@ -8,18 +8,22 @@ pub struct OutputBuffer {
 impl OutputBuffer {
   pub const LEN: usize = 1024;
 
-  pub fn bytes_to_read(&self) -> usize {
+  pub fn new() -> Self {
+    OutputBuffer { data: [0.0; Self::LEN], read_head: 0, write_head: 0 }
+  }
+
+  pub fn samples_to_read(&self) -> usize {
     ((self.write_head - self.read_head) + Self::LEN) % Self::LEN
   }
 
-  pub fn read(&mut self, n: usize) -> Vec<f32> {
-    if self.bytes_to_read() > n {
-      let slice = &self.data[self.read_head .. self.read_head + n].to_vec();
-      self.read_head = (self.read_head + n) % Self::LEN;
-      return slice.clone();
+  pub fn read(&mut self) -> f32 {
+    if self.samples_to_read() != 0 {
+      let sample = self.data[self.read_head];
+      self.read_head = (self.read_head + 1) % Self::LEN;
+      return sample;
     }
 
-    vec![]
+    0.0
   }
 
   pub fn write<'a, I>(&mut self, buffer: I)
